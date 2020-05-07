3adb553371a4a57b8ec7699b56069019ae32bea7
Dmitry Voronov <voronov.dmitry@gmail.com>
2020-05-07 12:02:01 -0400

    added Bootstrap carousel

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DemoCarousel from "./components/carousel"
+import BootstrapCarousel from "./components/bootstrapCarousel"
 function App() {
   return (
     <div className="App">
+
+      <BootstrapCarousel />
     
-      <DemoCarousel /> 
+      {/* <DemoCarousel />  */}
     </div>
   );
 }
